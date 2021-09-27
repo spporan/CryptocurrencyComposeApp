8ec5fa8eb0d80ed05a9d7bbfd1dcd742fedd7951
ridmik <poran.cse@gmail.com>
2021-09-28 01:46:08 +0600

    implemented GetCoinsUserCase.kt

--- app/src/main/java/com/poran/cryptocurrencycomposeapp/domain/usecases/getCoins/GetCoinsUserCase.kt
+++ app/src/main/java/com/poran/cryptocurrencycomposeapp/domain/usecases/getCoins/GetCoinsUserCase.kt
@@ -1,11 +1,13 @@
 package com.poran.cryptocurrencycomposeapp.domain.usecases.getCoins
 
+import android.util.Log
 import com.poran.cryptocurrencycomposeapp.common.Resource
 import com.poran.cryptocurrencycomposeapp.data.remote.dto.toCoin
 import com.poran.cryptocurrencycomposeapp.domain.models.Coin
 import com.poran.cryptocurrencycomposeapp.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import retrofit2.HttpException
 import java.io.IOException
 import java.lang.Exception
@@ -19,13 +21,20 @@ class GetCoinsUserCase @Inject constructor(
         try {
             emit(Resource.Loading())
             val coins = repository.getCoins()
+            Log.d("COIN","COIN: $coins")
             emit(Resource.Success(coins.map { it.toCoin() }))
         }
         catch (ex: HttpException) {
+            Log.d("COIN","error: ${ex.message()}")
+
             emit(Resource.Error<List<Coin>>(ex.localizedMessage?:"UnExcepted error occurred!", ))
         }
         catch (e: IOException) {
+            Log.d("COIN","error: ${e.localizedMessage}")
             emit(Resource.Error<List<Coin>>("Couldn't reach server. Please check your internet connection" ))
+        }
+        catch (e: Exception) {
+            Log.d("COIN","error: ${e.localizedMessage}")
         }
     }
 }
