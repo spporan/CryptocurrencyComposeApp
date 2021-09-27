c4d9a66084afe94f479bbf5059b8fb03ecdbff57
ridmik <poran.cse@gmail.com>
2021-09-28 01:47:01 +0600

    updated Constants.kt

--- app/src/main/java/com/poran/cryptocurrencycomposeapp/common/Constants.kt
+++ app/src/main/java/com/poran/cryptocurrencycomposeapp/common/Constants.kt
@@ -1,5 +1,6 @@
 package com.poran.cryptocurrencycomposeapp.common
 
 object Constants {
-
+    const val BASE_URL = "https://api.coinpaprika.com/"
+    const val PARAM_COIN_ID = "coinId"
 }
--- app/src/main/java/com/poran/cryptocurrencycomposeapp/common/Resource.kt
+++ app/src/main/java/com/poran/cryptocurrencycomposeapp/common/Resource.kt
@@ -6,7 +6,7 @@ package com.poran.cryptocurrencycomposeapp.common
     class Loading(data: T? = null): Resource<T>(data)
 }*/
 
-sealed class Resource<T>(private val data: T? = null, private val message: String? = null) {
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
     class Loading<T>( data: T? = null) : Resource<T>(data)
